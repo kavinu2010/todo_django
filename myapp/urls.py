f9ac3a0bfd710f906a92725c403aed43f20117c3
kavitha <kavinu2010@gmail.com>
2025-01-05 19:53:57 +0100

    class added in view page

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import TaskList
 
 urlpatterns=[
-  path('', views.hello,name='hello'),
+  path('', TaskList.as_view ,name='tasks'),
 ]
