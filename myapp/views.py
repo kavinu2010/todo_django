545b93f71021e4cbb0fd147ad1ffe25ed429207c
kavitha <kavinu2010@gmail.com>
2025-01-10 21:52:42 +0100

    task added for respective register user not wotking

--- myapp/views.py
+++ myapp/views.py
@@ -4,13 +4,14 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
 
 class CustomLoginView(LoginView):
     template_name='myapp/login.html'
-    fields='__all__'
+    fields = ['title','description','complete']
     redirect_authenticated_user = True
      
     def get_success_url(self):
@@ -20,27 +21,39 @@ class CustomLoginView(LoginView):
  
 
 # Create your views here.
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin,ListView):
     model=Task 
     context_object_name='tasks'
 
-class TaskDetail(DetailView):
+     
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['tasks']=context['tasks'].filter(user=self.request.user)
+    #     context['count']=context['tasks'].filter(complete=False).count()
+    #     return context
+
+class TaskDetail(LoginRequiredMixin,DetailView):
     model=Task
     context_object_name='task'
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model=Task 
-    fields = '__all__'
+    fields = ['title','description','complete']
     success_url=reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+    def form_invalid(self, form):
+        form.instance.user=self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model=Task 
-    fields = '__all__'
+    fields = ['title','description','complete']
     success_url=reverse_lazy('tasks')
     
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin,DeleteView):
     model=Task
     context_object_name='task'
     success_url=reverse_lazy('tasks')
--- todo/settings.py
+++ todo/settings.py
@@ -113,7 +113,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
